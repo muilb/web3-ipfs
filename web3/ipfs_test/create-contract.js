00f1a93ee4f71c24cbb5ea6a4d833eb433b6d7bd
muilb <lebamui@gmail.com>
2019-01-14 15:09:32 +0700

    check

--- web3/ipfs_test/create-contract.js
+++ web3/ipfs_test/create-contract.js
@@ -1,3 +1,12 @@
 /**
  * Created by lebamui on 13/01/2019.
  */
+require('./contract')("onlycheck", function (err, res) {
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        console.log(res.options,address);
+    }
+
+})
